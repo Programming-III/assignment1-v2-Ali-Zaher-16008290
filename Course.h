004d8c957ee68c9de75a87597376448dc3880405
Ali-Zaher-16008290 <ali.zaher@student.giu-uni.de>
2025-11-11 09:22:32 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -5,6 +5,18 @@
 #include "Student.h"
 using namespace std;
 //#write your code here
+class Course {
+private:
+    string courseName;
+	string courseCode;
+    int maxStudents;
+	Student* students;
+    int currentStudents;
+public:
+    Course(string cn, string cc, int ms);
+    void addStudent(const Student& s) ;
+    void display();
+};
 
 
 
